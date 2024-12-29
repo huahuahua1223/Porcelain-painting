7ee6c354a9a2e4d1f4aba157ba148e89151439f5
huahua <1494133104@qq.com>
2024-12-29 16:05:00 +0800

    test: useful

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -14,6 +14,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol"; // 防止重入�
 import "./IERC4907.sol"; // 导入 ERC4907 接口
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
+
 contract YourCollectible is
 	ERC721,
 	ERC721Enumerable,
